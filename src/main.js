759976a6aa865bca26af401c5a0e9192d3cd239f
frpa2201 <fredrikpaulsson03@gmail.com>
2025-12-17 15:45:55 +0100

    Added three.js code for rendering models as well as the city model.
    Co-authored-by: erbe2201 <erbe2201@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,23 +1,59 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+
+let object;
+
+let controls;
+
+let objToRender = 'city';
+
+const loader = new GLTFLoader();
+
+loader.load(
+    `models/${objToRender}/scene.gltf`,
+    function(gltf){
+        object = gltf.scene;
+        scene.add(object);
+    },
+    function(xhr){
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function(error){
+        console.log(error);
+    }
+);
+
+const renderer = new THREE.WebGLRenderer({alpha: true}); 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+camera.position.z = objToRender === 'city' ? 25 : 500;
 
-camera.position.z = 5;
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500,500,500);
+topLight.castShadow = true;
+scene.add(topLight);
+
+const ambientLight = new THREE.AmbientLight(0x333333, objToRender === 'city' ? 5 : 1);
+scene.add(ambientLight);
+
+controls = new OrbitControls(camera, renderer.domElement);
+
+document.onmousemove = (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+}
 
 function animate() {
+    requestAnimationFrame(animate);
     renderer.render( scene, camera );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 }
 renderer.setAnimationLoop( animate );
+
