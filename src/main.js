1c3f88a5e90a8618b14535b7f36693095c924696
frpa2201 <fredrikpaulsson03@gmail.com>
2025-12-19 16:06:08 +0100

    - Made the canvas always take up 100% width and height. - Fixed animate function to not render everything twice. - Put everything in onload function - Added onresize() function

--- src/main.js
+++ src/main.js
@@ -2,58 +2,68 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+function projectInit(){
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-let mouseX = window.innerWidth / 2;
-let mouseY = window.innerHeight / 2;
+    let mouseX = window.innerWidth / 2;
+    let mouseY = window.innerHeight / 2;
 
-let object;
+    let object;
 
-let controls;
+    let controls;
 
-let objToRender = 'ufo';
+    let objToRender = 'city';
 
-const loader = new GLTFLoader();
+    const loader = new GLTFLoader();
 
-loader.load(
-    `models/${objToRender}/scene.gltf`,
-    function(gltf){
-        object = gltf.scene;
-        scene.add(object);
-    },
-    function(xhr){
-        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-    function(error){
-        console.log(error);
+    loader.load(
+        `models/${objToRender}/scene.gltf`,
+        function(gltf){
+            object = gltf.scene;
+            scene.add(object);
+        },
+        function(xhr){
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        function(error){
+            console.log(error);
+        }
+    );
+
+    const renderer = new THREE.WebGLRenderer({alpha: true}); 
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    camera.position.set(350, 350, -350);
+
+
+    const topLight = new THREE.DirectionalLight(0xffffff, 1);
+    topLight.position.set(500,500,500);
+    topLight.castShadow = true;
+    scene.add(topLight);
+
+
+    const ambientLight = new THREE.AmbientLight(0x333333, 5);
+    scene.add(ambientLight);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+
+    document.onmousemove = (e) => {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
     }
-);
 
-const renderer = new THREE.WebGLRenderer({alpha: true}); 
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+    window.onresize = (e) => {
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        camera.aspect = window.innerWidth / window.innerHeight
+    }
 
-camera.position.z = objToRender === 'ufo' ? 25 : 500;
+    function animate() {
+        renderer.render( scene, camera );
+    }
+    renderer.setAnimationLoop( animate );
 
-const topLight = new THREE.DirectionalLight(0xffffff, 1);
-topLight.position.set(500,500,500);
-topLight.castShadow = true;
-scene.add(topLight);
-
-const ambientLight = new THREE.AmbientLight(0x333333, objToRender === 'ufo' ? 5 : 1);
-scene.add(ambientLight);
-
-controls = new OrbitControls(camera, renderer.domElement);
-
-document.onmousemove = (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render( scene, camera );
-}
-renderer.setAnimationLoop( animate );
-
+window.onload = projectInit;
